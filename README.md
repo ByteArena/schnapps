65b31f0535e187b839ef1dd3ab4e7e7696559536
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-10-10 07:53:45 +0200

    feat: add dns server

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # schnapps
+
+## Features
+
+- DNS server (only A records are supported)
+- QMP server
+- Manages a KVM process and its lifecycle
+
+## Roadmap
+
+- Network gateway (via tap/tun on the host)
---
+++ dns/server.go
@@ -0,0 +1,95 @@
+package dns
+
+import (
+	"fmt"
+
+	"github.com/miekg/dns"
+)
+
+type Records map[string]string
+type onRequestHook func(addr string)
+
+type Server struct {
+	addr          string
+	zone          string
+	records       Records
+	onRequestHook onRequestHook
+
+	stopChan chan bool
+}
+
+func MakeServer(addr, zone string, records Records) Server {
+
+	return Server{
+		addr:    addr,
+		zone:    zone,
+		records: records,
+
+		onRequestHook: func(addr string) {},
+
+		stopChan: make(chan bool),
+	}
+}
+
+func (s *Server) SetOnRequestHook(fn onRequestHook) {
+	s.onRequestHook = fn
+}
+
+func (s *Server) Start() error {
+	dns.HandleFunc(s.zone, s.handleDnsRequest)
+
+	server := &dns.Server{
+		Addr: s.addr,
+		Net:  "udp",
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		<-s.stopChan
+
+		server.Shutdown()
+		return
+	}()
+
+	return nil
+}
+
+func (s *Server) parseQuery(m *dns.Msg) {
+	for _, q := range m.Question {
+		switch q.Qtype {
+		case dns.TypeA:
+			s.onRequestHook(q.Name)
+
+			ip := s.records[q.Name]
+			if ip != "" {
+				rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+				if err == nil {
+					m.Answer = append(m.Answer, rr)
+				}
+			}
+		}
+	}
+}
+
+func (s *Server) handleDnsRequest(w dns.ResponseWriter, r *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Compress = false
+
+	switch r.Opcode {
+	case dns.OpcodeQuery:
+		s.parseQuery(m)
+	}
+
+	w.WriteMsg(m)
+}
+
+func (s *Server) Stop() {
+	s.stopChan <- true
+	close(s.stopChan)
+}
